1566e6452541ee8a7da22b9f159baab382fec08b
nskowron <nel.skowronek@gmail.com>
2025-01-03 13:00:21 +0000

    Added player

---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player
+{
+    public final int id;
+    public final Color color;
+
+    public Player(int id, Color color)
+    {
+        this.id = id;
+        this.color = color;
+    }
+}
