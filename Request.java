3d59c5fcc3d1e7963f55fb8aba4f3b5b8ba7ad21
nskowron <nel.skowronek@gmail.com>
2025-01-03 13:50:17 +0000

    request update

--- Request.java
+++ Request.java
@@ -1,4 +1,22 @@
-public interface Request
+public enum Request
 {
-    public Object getData();
+    MOVE
+    {
+        private Move move;
+
+        @Override
+        public void setData(Object data) throws ClassCastException
+        {
+            move = (Move)data;
+        }
+
+        @Override
+        public Object getData()
+        {
+            return move;
+        }
+    };
+
+    public abstract void setData(Object data);
+    public abstract Object getData();
 }
