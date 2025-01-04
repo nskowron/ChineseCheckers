127b7b4b2a36117cac5012ded29358ffd8da11ba
nskowron <nel.skowronek@gmail.com>
2025-01-04 13:11:44 +0000

    Added GameState class for update

---
+++ GameState.java
@@ -0,0 +1,28 @@
+import java.util.Map;
+
+public class GameState
+{
+    public final Map<String, Color> board;
+    public final Player currentTurn;
+    public final boolean won;
+
+    public GameState(Map<String, Node> nodes, Player currentTurn, boolean won)
+    {
+        this.currentTurn = currentTurn;
+        this.won = won;
+
+        Map<String, Color> boardColors = new HashMap<>();
+        for(Map.Entry<String, Node> node : nodes.entrySet())
+        {
+            Piece piece = node.getValue().getPiece();
+            if(piece == null)
+            {
+                boardColors.put(node.getKey(), Color.DEFAULT);
+            }
+            else
+            {
+                boardColors.put(node.getKey(), piece.getColor());
+            }
+        }
+    }
+}
--- Piece.java
+++ Piece.java
@@ -3,14 +3,28 @@ import java.io.Serializable;
 public class Piece implements Serializable
 {
     private final int ownerID;
+    private Color color;
 
+    public Piece(Color color)
+    {
+        this.color = color;
+        this.ownerID = -1;
+    }
+
+    @Deprecated
     public Piece(int ownerID)
     {
         this.ownerID = ownerID;
     }
 
+    @Deprecated
     public int getOwner()
     {
         return ownerID;
+    }
+
+    public Color getColor()
+    {
+        return color;
     }
 }
